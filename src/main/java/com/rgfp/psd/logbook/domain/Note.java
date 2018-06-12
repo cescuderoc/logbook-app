7824a72fb27ac3dab56ba3588e9456d2b9615642
Chris <ext_cescuderoc@falabella.cl>
2018-06-12 16:20:47 -0400

    Se edita html

--- src/main/java/com/rgfp/psd/logbook/domain/Note.java
+++ src/main/java/com/rgfp/psd/logbook/domain/Note.java
@@ -11,7 +11,7 @@ import javax.persistence.Table;
 import java.time.LocalDateTime;
 
 @Entity
-@Data
+/*@Data*/
 @Table(name = "notes")
 public class Note {
 
@@ -19,8 +19,47 @@ public class Note {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String title;
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
     private LocalDateTime timestamp;
     @Column(length=10000)
     private String content;
 
+    public String getSummary(){
+
+        String content =this.getContent();
+
+        return content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
--- src/test/java/com/rgfp/psd/logbook/domain/NoteTest.java
+++ src/test/java/com/rgfp/psd/logbook/domain/NoteTest.java
@@ -3,7 +3,22 @@ package com.rgfp.psd.logbook.domain;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class NoteTest {
+
+    @Test
+    public void metodoQuerecibeSummaryDevolviendoHasta240Caracteres() {
+
+        String texto = "christopernuevo";
+
+        Note note = new Note();
+
+        note.setContent(texto);
+
+        System.out.print("texto: "+texto.length());
+        assertTrue(note.getSummary().length()> 0);
+    }
+
 
 }
